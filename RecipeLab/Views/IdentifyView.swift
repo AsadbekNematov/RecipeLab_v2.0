89ccc66665c5c7d6391e570fd8fd41cf0085dc3a
Asadbek <asadbekn443@gmail.com>
2023-05-17 05:11:28 -0400

    working api for recipes

--- RecipeLab/Views/IdentifyView.swift
+++ RecipeLab/Views/IdentifyView.swift
@@ -43,93 +43,98 @@ struct IdentifyView: View {
     @State private var cancellables = Set<AnyCancellable>()
     @State private var recipeResults = [RecipeResponse]()
     @State private var showingRecipesPopup = false
+    @State private var showRecipesView = false
+
 
 
     
     var body: some View {
-        VStack {
-            HStack{
+        NavigationView {
+            
+            VStack {
+                HStack{
+                    
+                    // Search bar
+                    Image(systemName: "magnifyingglass").foregroundColor(.gray)
+                    
+                    TextField("Search Ingredient", text: $searchText)
+                        .onChange(of: searchText, perform: loadIngredients)
+                }                    .textFieldStyle(OvalTextFieldStyle())
                 
-                // Search bar
-                Image(systemName: "magnifyingglass").foregroundColor(.gray)
                 
-                TextField("Search Ingredient", text: $searchText)
-                    .onChange(of: searchText, perform: loadIngredients)
-            }                    .textFieldStyle(OvalTextFieldStyle())
-            
-            
-                .padding()
-            Spacer()
-            
-            
-            // Display search results
-            if !searchResults.isEmpty {
-                Text("Search Results:")
-                List(searchResults) { ingredient in
-                    HStack {
-                        URLImage(url: "https://spoonacular.com/cdn/ingredients_100x100/\(ingredient.image)")
-                        Text(ingredient.name.capitalized)
-                            .textInputAutocapitalization(.sentences)
-                        
-                    }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        ingredientList.append(ingredient)
-                        searchResults.removeAll()
-                        searchText = ""
-                    }
-                }
-            }
-            
-            // Display chosen ingredients
-            if !ingredientList.isEmpty {
-                Text("Your Ingredients:")
-                List {
-                    ForEach(ingredientList.indices, id: \.self) { index in
+                    .padding()
+                Spacer()
+                
+                
+                // Display search results
+                if !searchResults.isEmpty {
+                    Text("Search Results:")
+                    List(searchResults) { ingredient in
                         HStack {
-                            URLImage(url: "https://spoonacular.com/cdn/ingredients_100x100/\(ingredientList[index].image)")
-                            Text(ingredientList[index].name.capitalized)
+                            URLImage(url: "https://spoonacular.com/cdn/ingredients_100x100/\(ingredient.image)")
+                            Text(ingredient.name.capitalized)
                                 .textInputAutocapitalization(.sentences)
                             
-                            Spacer()
-                            Button(action: {
-                                ingredientList.remove(at: index)
-                                
-                            }) {
-                                Image(systemName: "trash")
-                            }
-                            .foregroundColor(.red)
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            ingredientList.append(ingredient)
+                            searchResults.removeAll()
+                            searchText = ""
+                            loadRecipes() // add this line
+
                         }
                     }
-                    .onDelete(perform: deleteIngredient)
                 }
-            }
-            Button {
-                loadRecipes()
-                showingRecipesPopup = true
-
-                // present the recipes in a pop-up window
-                // I'll leave the UI details up to you
-            } label: {
-                HStack{
-                    Text("Recipes")
-                    Image(systemName: "fork.knife")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                }.padding(.all, 15.0)
+                
+                // Display chosen ingredients
+                if !ingredientList.isEmpty {
+                    Text("Your Ingredients:")
+                    List {
+                        ForEach(ingredientList.indices, id: \.self) { index in
+                            HStack {
+                                URLImage(url: "https://spoonacular.com/cdn/ingredients_100x100/\(ingredientList[index].image)")
+                                Text(ingredientList[index].name.capitalized)
+                                    .textInputAutocapitalization(.sentences)
+                                
+                                Spacer()
+                                Button(action: {
+                                    ingredientList.remove(at: index)
+                                    
+                                }) {
+                                    Image(systemName: "trash")
+                                }
+                                .foregroundColor(.red)
+                            }
+                        }
+                        .onDelete(perform: deleteIngredient)
+                    }
+                }
+              NavigationLink(destination: RecipePopupView(recipes: convertToPopupRecipes(recipeResults))) {
+                    HStack{
+                        Text("Recipes")
+                        Image(systemName: "fork.knife")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding(.all, 15.0)
                     .frame(width: 300.0)
                     .foregroundColor(.black)
                     .background(.orange)
                     .cornerRadius(/*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                     .scenePadding(.horizontal)
+                }
+            }.sheet(isPresented: $showingRecipesPopup) {
+                RecipePopupView(recipes: convertToPopupRecipes(recipeResults))
             }
-        }.sheet(isPresented: $showingRecipesPopup) {
-            RecipePopupView(recipes: recipeResults)
         }
-
     }
-    
+    func convertToPopupRecipes(_ recipes: [RecipeResponse]) -> [PopupRecipe] {
+        recipes.map { PopupRecipe(id: $0.id, title: $0.title, image: $0.image, missedIngredients: $0.missedIngredients) }
+    }
+
+
 
     func loadRecipes() {
         let ingredientNames = ingredientList.map { $0.name }.joined(separator: ",+")
@@ -144,6 +149,8 @@ struct IdentifyView: View {
                 self.recipeResults = response
             })
             .store(in: &cancellables)
+        self.showRecipesView = true
+
     }
 
     
@@ -154,7 +161,8 @@ struct IdentifyView: View {
         }
     }
     func loadIngredients(_ searchText: String) {
-        let url = URL(string: "https://api.spoonacular.com/food/ingredients/search?query=\(searchText)&number=10&apiKey=e51d14bc19d6447d8c63c52f7a62e9e1")!
+        guard let encodedSearchText = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let url = URL(string: "https://api.spoonacular.com/food/ingredients/search?query=\(encodedSearchText)&number=10&apiKey=e51d14bc19d6447d8c63c52f7a62e9e1")!
         
         URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data }
@@ -166,6 +174,7 @@ struct IdentifyView: View {
             })
             .store(in: &cancellables)
     }
+
     
 }
 
@@ -175,56 +184,26 @@ struct IdentifyView_Previews: PreviewProvider {
     }
 }
 
-struct URLImage: View {
-    @State private var uiImage: UIImage? = nil
-    let url: String
-    
-    var body: some View {
-        Group {
-            if let image = self.uiImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(50)
-            } else {
-                ProgressView()
-            }
-        }.onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        guard let imageURL = URL(string: url) else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: imageURL) { data, response, error in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.uiImage = image
-                }
-            }
-        }.resume()
-    }
-}
 
-struct RecipePopupView: View {
-    let recipes: [RecipeResponse]
 
-    var body: some View {
-        NavigationView {
-            List(recipes) { recipe in
-                VStack(alignment: .leading) {
-                    Text(recipe.title)
-                        .font(.headline)
-                    URLImage(url: recipe.image)
-                    Text("Missing Ingredients:")
-                    ForEach(recipe.missedIngredients) { ingredient in
-                        Text(ingredient.name)
-                    }
-                }
-            }
-            .navigationBarTitle("Recipes", displayMode: .inline)
-        }
-    }
-}
+//struct RecipePopupView: View {
+//    let recipes: [RecipeResponse]
+//
+//    var body: some View {
+//        NavigationView {
+//            List(recipes) { recipe in
+//                VStack(alignment: .leading) {
+//                    Text(recipe.title)
+//                        .font(.headline)
+//                    URLImage(url: recipe.image)
+//                    Text("Missing Ingredients:")
+//                    ForEach(recipe.missedIngredients) { ingredient in
+//                        Text(ingredient.name)
+//                    }
+//                }
+//            }
+//            .navigationBarTitle("Recipes", displayMode: .inline)
+//        }
+//    }
+//}
 
